b20da890d55d557b2cd255e421d890785958a853
Shannon <sdeminick@gmail.com>
2017-10-04 13:54:50 +1100

    Auto generate the secret for now

--- src/Umbraco.RestApi/Security/UmbracoAuthorizationServerProviderOptions.cs
+++ src/Umbraco.RestApi/Security/UmbracoAuthorizationServerProviderOptions.cs
@@ -1,12 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Text;
 using System.Web.Cors;
+using ClientDependency.Core;
 using Microsoft.AspNet.Identity.Owin;
 
 namespace Umbraco.RestApi.Security
 {
     public class UmbracoAuthorizationServerProviderOptions
     {
+
         /// <summary>
         /// Default options allows all request, CORS does not limit anything
         /// </summary>
@@ -21,6 +24,17 @@ namespace Umbraco.RestApi.Security
                 AllowAnyOrigin = true,
                 SupportsCredentials = true
             };
+
+            //set the secret based on the current website
+
+            var timezone = TimeZone.CurrentTimeZone.StandardName;
+            var directory = System.Web.HttpRuntime.BinDirectory;
+            var machineName = Environment.MachineName;
+            var appDomainAppId = System.Web.HttpRuntime.AppDomainAppId;
+            Secret = string.Concat(timezone, directory, machineName, appDomainAppId).GenerateHash();
+
+            //for now we only have one Audience
+            Audience = "UmbracoRestApi";
         }
 
         /// <summary>
@@ -28,15 +42,18 @@ namespace Umbraco.RestApi.Security
         /// </summary>
         public bool AllowInsecureHttp { get; set; }
 
+        //TODO: The Secret and Audience need to be turned into an IAudienceStore or IClientStore 
+        //Need to follow more of this for http://bitoftech.net/2014/10/27/json-web-token-asp-net-web-api-2-jwt-owin-authorization-server/ dealing with AudienceStores
+
         /// <summary>
         /// This is the key for the client
         /// </summary>
-        public string Secret { get; set; }
+        internal string Secret { get; set; }
 
         /// <summary>
         /// This is a "ClientId"
         /// </summary>
-        public string Audience { get; set; }
+        internal string Audience { get; set; }
 
         public string AuthEndpoint { get; set; } = "/umbraco/rest/oauth/token";
         public CorsPolicy CorsPolicy { get; set; }
