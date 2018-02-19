b8f1a57425339dbd68d005935d5fe99e79aeef9f
Claus <claus@umbraco.com>
2018-02-19 14:53:48 +0100

    wip - adding depth param.

--- src/Umbraco.RestApi/Controllers/PublishedContentController.cs
+++ src/Umbraco.RestApi/Controllers/PublishedContentController.cs
@@ -12,6 +12,7 @@ using Umbraco.Core;
 using Umbraco.Core.Configuration;
 using Umbraco.Core.Models;
 using Umbraco.RestApi.Models;
+using Umbraco.RestApi.Models.Mapping;
 using Umbraco.RestApi.Routing;
 using Umbraco.RestApi.Security;
 using Umbraco.Web;
@@ -52,39 +53,39 @@ namespace Umbraco.RestApi.Controllers
 
         [HttpGet]
         [CustomRoute("")]
-        public virtual HttpResponseMessage Get()
+        public virtual HttpResponseMessage Get(int depth = PublishedContentMapper.DefaultDepth)
         {
             var rootContent = Umbraco.TypedContentAtRoot().ToArray();
             if (rootContent.Length > 0)
                 PcrFactory.Create(rootContent[0], Request.RequestUri);
 
-            var result = AutoMapper.Mapper.Map<IEnumerable<PublishedContentRepresentation>>(rootContent).ToList();
+            var result = AutoMapper.Mapper.Map<IEnumerable<PublishedContentRepresentation>>(rootContent, options => options.Items["prop::depth"] = depth).ToList();
             var representation = new PublishedContentListRepresenation(result);
             return Request.CreateResponse(HttpStatusCode.OK, representation);
         }
 
         [HttpGet]
         [CustomRoute("{id:int}")]
-        public HttpResponseMessage Get(int id)
+        public HttpResponseMessage Get(int id, int depth = PublishedContentMapper.DefaultDepth)
         {
-            return GetInternal(() => Umbraco.TypedContent(id));
+            return GetInternal(() => Umbraco.TypedContent(id), depth);
         }
 
         [HttpGet]
         [CustomRoute("{id:guid}")]
-        public HttpResponseMessage Get(Guid id)
+        public HttpResponseMessage Get(Guid id, int depth = PublishedContentMapper.DefaultDepth)
         {
-            return GetInternal(() => Umbraco.TypedContent(id));
+            return GetInternal(() => Umbraco.TypedContent(id), depth);
         }
 
-        private HttpResponseMessage GetInternal(Func<IPublishedContent> getContent)
+        private HttpResponseMessage GetInternal(Func<IPublishedContent> getContent, int depth)
         {
             var content = getContent();
             if (content == null) return Request.CreateResponse(HttpStatusCode.NotFound);
 
             PcrFactory.Create(content, Request.RequestUri);
 
-            var result = AutoMapper.Mapper.Map<PublishedContentRepresentation>(content);
+            var result = AutoMapper.Mapper.Map<PublishedContentRepresentation>(content, options => options.Items["prop::depth"] = depth);
 
             return result == null
                 ? Request.CreateResponse(HttpStatusCode.NotFound)
@@ -95,20 +96,21 @@ namespace Umbraco.RestApi.Controllers
         [CustomRoute("{id:int}/children")]
         public HttpResponseMessage GetChildren(int id,
             [System.Web.Http.ModelBinding.ModelBinder(typeof(PagedQueryModelBinder))]
-            PagedQuery query)
+            PagedQuery query,
+            int depth = PublishedContentMapper.DefaultDepth)
         {
-            return GetChildren(() => Umbraco.TypedContent(id), query);
+            return GetChildren(() => Umbraco.TypedContent(id), query, depth);
         }
 
         [HttpGet]
         [CustomRoute("{id:guid}/children")]
         public HttpResponseMessage GetChildren(Guid id,
-            [System.Web.Http.ModelBinding.ModelBinder(typeof(PagedQueryModelBinder))] PagedQuery query)
+            [System.Web.Http.ModelBinding.ModelBinder(typeof(PagedQueryModelBinder))] PagedQuery query, int depth = PublishedContentMapper.DefaultDepth)
         {
-            return GetChildren(() => Umbraco.TypedContent(id), query);
+            return GetChildren(() => Umbraco.TypedContent(id), query, depth);
         }
 
-        private HttpResponseMessage GetChildren(Func<IPublishedContent> getContent, PagedQuery query)
+        private HttpResponseMessage GetChildren(Func<IPublishedContent> getContent, PagedQuery query, int depth)
         {
             var content = getContent();
             if (content == null) return Request.CreateResponse(HttpStatusCode.NotFound);
@@ -119,7 +121,10 @@ namespace Umbraco.RestApi.Controllers
             var total = resolved.Length;
             var pages = (total + query.PageSize - 1) / query.PageSize;
 
-            var items = AutoMapper.Mapper.Map<IEnumerable<PublishedContentRepresentation>>(resolved.Skip(ContentControllerHelper.GetSkipSize(query.Page - 1, query.PageSize)).Take(query.PageSize)).ToList();
+            var items = AutoMapper.Mapper.Map<IEnumerable<PublishedContentRepresentation>>(resolved
+                    .Skip(ContentControllerHelper.GetSkipSize(query.Page - 1, query.PageSize))
+                    .Take(query.PageSize),
+                options => options.Items["prop::depth"] = depth).ToList();
             var result = new PublishedContentPagedListRepresentation(items, total, pages, query.Page, query.PageSize, LinkTemplates.PublishedContent.PagedChildren, new { id = content.GetKey() });
 
             return Request.CreateResponse(HttpStatusCode.OK, result);
@@ -129,20 +134,21 @@ namespace Umbraco.RestApi.Controllers
         [CustomRoute("{id:int}/descendants/")]
         public HttpResponseMessage GetDescendants(int id,
             [System.Web.Http.ModelBinding.ModelBinder(typeof(PagedQueryModelBinder))]
-            PagedQuery query)
+            PagedQuery query,
+            int depth = PublishedContentMapper.DefaultDepth)
         {
-            return GetDescendantsInternal(() => Umbraco.TypedContent(id), query);
+            return GetDescendantsInternal(() => Umbraco.TypedContent(id), query, depth);
         }
 
         [HttpGet]
         [CustomRoute("{id:guid}/descendants/")]
         public HttpResponseMessage GetDescendants(Guid id,
-            [System.Web.Http.ModelBinding.ModelBinder(typeof(PagedQueryModelBinder))] PagedQuery query)
+            [System.Web.Http.ModelBinding.ModelBinder(typeof(PagedQueryModelBinder))] PagedQuery query, int depth = PublishedContentMapper.DefaultDepth)
         {
-            return GetDescendantsInternal(() => Umbraco.TypedContent(id), query);
+            return GetDescendantsInternal(() => Umbraco.TypedContent(id), query, depth);
         }
 
-        private HttpResponseMessage GetDescendantsInternal(Func<IPublishedContent> getContent, PagedQuery query)
+        private HttpResponseMessage GetDescendantsInternal(Func<IPublishedContent> getContent, PagedQuery query, int depth)
         {
             var content = getContent();
             if (content == null) return Request.CreateResponse(HttpStatusCode.NotFound);
@@ -153,7 +159,10 @@ namespace Umbraco.RestApi.Controllers
 
             var total = resolved.Length;
             var pages = (total + query.PageSize - 1) / query.PageSize;
-            var items = AutoMapper.Mapper.Map<IEnumerable<PublishedContentRepresentation>>(resolved.Skip(ContentControllerHelper.GetSkipSize(query.Page - 1, query.PageSize)).Take(query.PageSize)).ToList();
+            var items = AutoMapper.Mapper.Map<IEnumerable<PublishedContentRepresentation>>(resolved
+                    .Skip(ContentControllerHelper.GetSkipSize(query.Page - 1, query.PageSize))
+                    .Take(query.PageSize),
+                options => options.Items["prop::depth"] = depth).ToList();
             var result = new PublishedContentPagedListRepresentation(items, total, pages, query.Page, query.PageSize, LinkTemplates.PublishedContent.PagedDescendants, new { id = content.GetKey() });
             return Request.CreateResponse(HttpStatusCode.OK, result);
         }
@@ -162,20 +171,22 @@ namespace Umbraco.RestApi.Controllers
         [CustomRoute("{id:int}/ancestors/{page?}/{pageSize?}")]
         public HttpResponseMessage GetAncestors(int id,
             [System.Web.Http.ModelBinding.ModelBinder(typeof(PagedQueryModelBinder))]
-            PagedQuery query)
+            PagedQuery query,
+            int depth = PublishedContentMapper.DefaultDepth)
         {
-            return GetAncestorsInternal(() => Umbraco.TypedContent(id), query);
+            return GetAncestorsInternal(() => Umbraco.TypedContent(id), query, depth);
         }
 
         [HttpGet]
         [CustomRoute("{id:guid}/ancestors/{page?}/{pageSize?}")]
         public HttpResponseMessage GetAncestors(Guid id,
-            [System.Web.Http.ModelBinding.ModelBinder(typeof(PagedQueryModelBinder))] PagedQuery query)
+            [System.Web.Http.ModelBinding.ModelBinder(typeof(PagedQueryModelBinder))] PagedQuery query,
+            int depth = PublishedContentMapper.DefaultDepth)
         {
-            return GetAncestorsInternal(() => Umbraco.TypedContent(id), query);
+            return GetAncestorsInternal(() => Umbraco.TypedContent(id), query, depth);
         }
 
-        private HttpResponseMessage GetAncestorsInternal(Func<IPublishedContent> getContent, PagedQuery query)
+        private HttpResponseMessage GetAncestorsInternal(Func<IPublishedContent> getContent, PagedQuery query, int depth)
         {
             var content = getContent();
             if (content == null) return Request.CreateResponse(HttpStatusCode.NotFound);
@@ -187,7 +198,10 @@ namespace Umbraco.RestApi.Controllers
             var total = resolved.Length;
             var pages = (total + query.PageSize - 1) / query.PageSize;
 
-            var items = AutoMapper.Mapper.Map<IEnumerable<PublishedContentRepresentation>>(resolved.Skip(ContentControllerHelper.GetSkipSize(query.Page - 1, query.PageSize)).Take(query.PageSize)).ToList();
+            var items = AutoMapper.Mapper.Map<IEnumerable<PublishedContentRepresentation>>(resolved
+                    .Skip(ContentControllerHelper.GetSkipSize(query.Page - 1, query.PageSize))
+                    .Take(query.PageSize),
+                options => options.Items["prop::depth"] = depth).ToList();
             var result = new PublishedContentPagedListRepresentation(items, total, pages, query.Page, query.PageSize, LinkTemplates.PublishedContent.PagedAncestors, new { id = content.GetKey() });
             return Request.CreateResponse(HttpStatusCode.OK, result);
         }
@@ -196,17 +210,21 @@ namespace Umbraco.RestApi.Controllers
         [CustomRoute("query")]
         public HttpResponseMessage GetQuery(
             [System.Web.Http.ModelBinding.ModelBinder(typeof(PagedQueryModelBinder))]
-            PagedQuery query)
+            PagedQuery query,
+            int depth = PublishedContentMapper.DefaultDepth)
         {
-            return GetQuery(query, 0);
+            return GetQuery(query, 0, depth);
         }
 
+
+        //TODO: Test if this can be called or is it hidden??
         [HttpGet]
         [CustomRoute("query/{id:int}")]
         public HttpResponseMessage GetQuery(
             [System.Web.Http.ModelBinding.ModelBinder(typeof(PagedQueryModelBinder))]
             PagedQuery query,
-            int id)
+            int id,
+            int depth = PublishedContentMapper.DefaultDepth)
         {
             var rootQuery = "";
             if (id > 0)
@@ -233,7 +251,7 @@ namespace Umbraco.RestApi.Controllers
 
             var paged = result.Skip((int)skip).Take(take);
             var pages = (result.Length + query.PageSize - 1) / query.PageSize;
-            var items = AutoMapper.Mapper.Map<IEnumerable<PublishedContentRepresentation>>(paged).ToList();
+            var items = AutoMapper.Mapper.Map<IEnumerable<PublishedContentRepresentation>>(paged, options => options.Items["prop::depth"] = depth).ToList();
             var representation = new PublishedContentPagedListRepresentation(items, result.Length, pages, query.Page, query.PageSize, LinkTemplates.PublishedContent.Query, new { id = key, query = query.Query, pageSize = query.PageSize });
 
             return Request.CreateResponse(HttpStatusCode.OK, representation);
@@ -243,7 +261,8 @@ namespace Umbraco.RestApi.Controllers
         [CustomRoute("query/{id:guid}")]
         public HttpResponseMessage GetQuery(
             [System.Web.Http.ModelBinding.ModelBinder(typeof(PagedQueryModelBinder))] PagedQuery query,
-            Guid id)
+            Guid id,
+            int depth = PublishedContentMapper.DefaultDepth)
         {
             //we will convert to INT here because the GUID lookup in xpath is slow until we fix this https://github.com/umbraco/Umbraco-CMS/pull/2367
             var intId = id != Guid.Empty ? Services.EntityService.GetIdForKey(id, UmbracoObjectTypes.Document) : Attempt.Succeed(0);
@@ -256,7 +275,8 @@ namespace Umbraco.RestApi.Controllers
         [CustomRoute("search")]
         public HttpResponseMessage Search(
             [System.Web.Http.ModelBinding.ModelBinder(typeof(PagedQueryModelBinder))]
-            PagedQuery query)
+            PagedQuery query,
+            int depth = PublishedContentMapper.DefaultDepth)
         {
             if (query.Query.IsNullOrWhiteSpace()) throw new HttpResponseException(HttpStatusCode.NotFound);
 
@@ -267,7 +287,7 @@ namespace Umbraco.RestApi.Controllers
             var result = Umbraco.ContentQuery.TypedSearch(SearchProvider.CreateSearchCriteria().RawQuery(query.Query), _searchProvider).ToArray();
             var paged = result.Skip(ContentControllerHelper.GetSkipSize(query.Page - 1, query.PageSize)).Take(query.PageSize);
             var pages = (result.Length + query.PageSize - 1) / query.PageSize;
-            var items = AutoMapper.Mapper.Map<IEnumerable<PublishedContentRepresentation>>(paged).ToList();
+            var items = AutoMapper.Mapper.Map<IEnumerable<PublishedContentRepresentation>>(paged, options => options.Items["prop::depth"] = depth).ToList();
             var representation = new PublishedContentPagedListRepresentation(items, result.Length, pages, query.Page, query.PageSize, LinkTemplates.PublishedContent.Search, new { query = query.Query, pageSize = query.PageSize });
 
             return Request.CreateResponse(HttpStatusCode.OK, representation);
@@ -275,14 +295,14 @@ namespace Umbraco.RestApi.Controllers
 
         [HttpGet]
         [CustomRoute("url")]
-        public HttpResponseMessage GetByUrl(string url)
+        public HttpResponseMessage GetByUrl(string url, int depth = PublishedContentMapper.DefaultDepth)
         {
             var content = UmbracoContext.ContentCache.GetByRoute(url);
             if (content == null) return Request.CreateResponse(HttpStatusCode.NotFound);
 
             PcrFactory.Create(content, Request.RequestUri);
 
-            var result = AutoMapper.Mapper.Map<PublishedContentRepresentation>(content);
+            var result = AutoMapper.Mapper.Map<PublishedContentRepresentation>(content, options => options.Items["prop::depth"] = depth);
 
             return result == null
                 ? Request.CreateResponse(HttpStatusCode.NotFound)
@@ -291,7 +311,7 @@ namespace Umbraco.RestApi.Controllers
 
         [HttpGet]
         [CustomRoute("tag/{tag}")]
-        public HttpResponseMessage GetByTag(string tag, string group = null, int page = 0, int size = 100)
+        public HttpResponseMessage GetByTag(string tag, string group = null, int page = 0, int size = 100, int depth = PublishedContentMapper.DefaultDepth)
         {
             var content = Umbraco.TagQuery.GetContentByTag(tag, group).ToArray();
 
@@ -302,7 +322,7 @@ namespace Umbraco.RestApi.Controllers
             var total = content.Length;
             var pages = (total + size - 1) / size;
 
-            var items = AutoMapper.Mapper.Map<IEnumerable<PublishedContentRepresentation>>(content.Skip(skip).Take(size)).ToList();
+            var items = AutoMapper.Mapper.Map<IEnumerable<PublishedContentRepresentation>>(content.Skip(skip).Take(size), options => options.Items["prop::depth"] = depth).ToList();
             var representation = new PublishedContentPagedListRepresentation(items, total, pages, page, size, LinkTemplates.PublishedContent.Search, new
             {
                 tag = tag,
